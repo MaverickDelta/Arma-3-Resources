9db648ceb932abac29d22c60bb02e2aef334ca22
Maverick Delta <68787583+MaverickDelta@users.noreply.github.com>
2021-01-01 02:19:51 +0000

    Delete script_macros.hpp

--- VehicleRepair.Altis/script_macros.hpp
+++
@@ -1,5 +0,0 @@
-
-
-
-//Condition Macros
-#define KINDOF_ARRAY(a,b) [##a,##b] call {_veh = _this select 0;_types = _this select 1;_res = false; {if (_veh isKindOf _x) exitWith { _res = true };} forEach _types;_res}
